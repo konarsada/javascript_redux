70164e37ac65429f0988286beacac5d27dd8b138
Sadabrata Konar <sadabrata.konar@airtel.com>
2024-05-25 00:15:22 +0530

    async actions

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
 
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -54,26 +57,16 @@ const rootReducer = combineReducers({
     icecream: icecreamReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 console.log('Initial state', store.getState())
 
-const unsubscribe = store.subscribe(
-    () => console.log(
-        "Updated state",
-        store.getState()
-    )
-)
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyIcecream())
 store.dispatch(buyIcecream())
-
-console.log(store.getState().cake)
 
 unsubscribe()
-
-
-// start video 6
